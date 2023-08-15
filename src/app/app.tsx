f68b53f403760e50395a1eb1484915f5a3bbb59b
Attila Csanyi <attila.csnyi@yahoo.co.uk>
2023-08-15 18:07:17 +0200

    feat: radix connect logic with RDT

--- src/app/app.tsx
+++ src/app/app.tsx
@@ -1,12 +1,34 @@
-// eslint-disable-next-line @typescript-eslint/no-unused-vars
+import { RadixNetwork } from '@radixdlt/babylon-gateway-api-sdk';
+import {
+  RadixDappToolkit
+} from '@radixdlt/radix-dapp-toolkit';
 
+declare global {
+  // eslint-disable-next-line @typescript-eslint/no-namespace
+  namespace JSX {
+    interface IntrinsicElements {
+      'radix-connect-button': React.DetailedHTMLProps<
+        React.HTMLAttributes<HTMLElement>,
+        HTMLElement
+      >
+    }
+  }
+}
 
-export function App() {
+export const App = () => {
+
+  const rdt = RadixDappToolkit({
+    dAppDefinitionAddress:
+      'account_tdx_d_12x5xkt4nzdqsym80c6sn32rx63efgyfcyj2ly7fcj0qcy85mh98d3p',
+    networkId: RadixNetwork.RCnetV2,
+  });
+
   return (
-    <div>
-      Radix React Nx Demo
+    <div className='header'>
+      <h1>Radix React Nx Demo</h1>
+      <radix-connect-button />
     </div>
   );
-}
+};
 
 export default App;
